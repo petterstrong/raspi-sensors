bd39fb2eaf2ed8e434fd3b42b90ae6ccf31e6814
zhangming <petterstrong@163.com>
2019-08-13 22:22:39 +0800

    添加继电器模块

---
+++ trigger.py
@@ -0,0 +1,16 @@
+import RPI.GPIO as gpio
+import sys
+
+channel = 4
+gpio.setmode(gpio.BCM)
+args = sys.argv
+ctl = args[1]
+
+if (int(ctl) == 1):
+    gpio.setup(channel, gpio.OUT)
+    gpio.output(channel, gpio.HIGH)
+
+if (int(ctl) == 0):
+    gpio.setup(channel, gpio.OUT)
+    gpio.output(channel, gpio.LOW)
+
